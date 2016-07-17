9fa0cda31d15afab5485cb047ffe1e39f53ca76f
Riveascore <ChrisAga.dev@gmail.com>
2016-07-17 03:33:47 -0500

    Add average reference line and show average number

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,5 +1,11 @@
+import _ from 'lodash';
 import React from 'react'
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+  const initialAverage = _.sum(data) / data.length;
+  return _.round(initialAverage);
+}
 
 export default (props) => {
   const listData = props.cityData.list;
@@ -9,7 +15,11 @@ export default (props) => {
     <td>
       <Sparklines height={120} width={180} data={chartData}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>
+        {average(chartData)}
+      </div>
     </td>
   );
 }
