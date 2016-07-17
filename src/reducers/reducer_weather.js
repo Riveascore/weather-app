591f608c1f226e82f81f5ea0076c1879d21467e6
Riveascore <ChrisAga.dev@gmail.com>
2016-07-17 02:15:11 -0500

    Concat new city data to current state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,14 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+const defaultState = {
+  city: "No City"
+}
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // same as state.concat(action.payload.data)
+      return [ action.payload.data, ...state ];
+  }
   return state;
 }
